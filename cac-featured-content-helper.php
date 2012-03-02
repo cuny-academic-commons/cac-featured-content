be891ba3bd47adba971ed7d4d8c7e035a5a7ed27
Boone Gorges <boonebgorges@gmail.com>
2012-03-02 16:36:17 -0500

    Fixes getPostBySlug() to use WP_Query properly

--- cac-featured-content-helper.php
+++ cac-featured-content-helper.php
@@ -46,13 +46,18 @@ class CACFeaturedContentHelper {
 	 * @return object
 	 */
 	function getPostBySlug($slug, $blog_id) {
+		global $post;
+
 		switch_to_blog($blog_id);
 
 	       	$posts = new WP_Query( array( 'name' => $slug ) );
 
-	       	foreach($posts as $post) {
-			if ( $post->post_name == $slug ) {
-				$single_post = $post;
+		if ( $posts->have_posts() ) {
+			while ( $posts->have_posts() ) {
+				$posts->the_post();
+				if ( $post->post_title == $slug ) {
+					$single_post = $post;
+				}
 			}
 		}
 
